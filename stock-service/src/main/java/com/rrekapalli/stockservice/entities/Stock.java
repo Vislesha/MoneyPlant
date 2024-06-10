b0eebd918ec23d6e24d35f4e4e61ad9f9078b023
VisleshaOrg <raja.rekapalli@outlook.com>
2024-06-10 13:27:50 +0530

    Dcokerization of services with Jib plugin complete

--- stock-service/src/main/java/com/rrekapalli/stockservice/entities/Stock.java
+++ stock-service/src/main/java/com/rrekapalli/stockservice/entities/Stock.java
@@ -5,12 +5,14 @@ import com.rrekapalli.moneyplantcore.entities.BaseAuditEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Nationalized;
 
 @Entity
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 public class Stock extends BaseAuditEntity {
     @Nationalized
     @Column(nullable = false)
