33a75dc81cde32e03a99306a177d0d086877ce51
VisleshaOrg <raja.rekapalli@outlook.com>
2024-06-11 12:30:07 +0530

    Swagger Open Api Documentation implemented

--- api-gateway/src/main/java/com/rrekapalli/routes/Routes.java
+++ api-gateway/src/main/java/com/rrekapalli/routes/Routes.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -16,6 +18,15 @@ public class Routes {
         return GatewayRouterFunctions.route("stock_service")
                 .route(RequestPredicates.path("/api/stock"),
                         HandlerFunctions.http("http://localhost:9740"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> stockServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("stock_service_swagger")
+                .route(RequestPredicates.path("/aggregate/stock-service/v1/api-docs"),
+                        HandlerFunctions.http("http://localhost:9740"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
@@ -28,6 +39,15 @@ public class Routes {
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> portfolioServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("portfolio_service_swagger")
+                .route(RequestPredicates.path("/aggregate/portfolio-service/v1/api-docs"),
+                        HandlerFunctions.http("http://localhost:9750"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
 
     @Bean
     public RouterFunction<ServerResponse> transactionServiceRoute() {
@@ -37,12 +57,30 @@ public class Routes {
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> transactionServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("transaction_service_swagger")
+                .route(RequestPredicates.path("/aggregate/transaction-service/v1/api-docs"),
+                        HandlerFunctions.http("http://localhost:9760"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
 
     @Bean
     public RouterFunction<ServerResponse> watchlistServiceRoute() {
         return GatewayRouterFunctions.route("watchlist_service")
                 .route(RequestPredicates.path("/api/watchlist"),
                         HandlerFunctions.http("http://localhost:9770"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> watchlistServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("watchlist_service_swagger")
+                .route(RequestPredicates.path("/aggregate/watchlist-service/v1/api-docs"),
+                        HandlerFunctions.http("http://localhost:9770"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 }
